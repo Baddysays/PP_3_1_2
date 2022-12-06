add96afac9825677ede45e62c36b722b73e85a25
Baddysays <vrm1457@yandex.ru>
2022-12-06 18:55:09 +0300

    поправил

--- src/main/java/web/service/UserServiceImpl.java
+++ src/main/java/web/service/UserServiceImpl.java
@@ -1,7 +1,6 @@
 package web.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import web.DAO.UserDao;
@@ -9,7 +8,6 @@ import web.model.User;
 
 import java.util.List;
 @Service
-@Component
 public class UserServiceImpl implements UserService {
     private final UserDao userDao;
     @Autowired
